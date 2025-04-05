d059d4791c8d98142e4483f5124af8a0c3bb2545
Laura Tavares <144948480+LauraTavares2323@users.noreply.github.com>
2025-04-04 22:34:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let nome1 = prompt("Qual seu primeiro nome?")
+console.log(nome1)
+
+let nome2 = prompt("Qual seu segundo nome?")
+console.log(nome2)
+
+let idade = prompt("Qual sua idade?")
+console.log(idade)
+
+let estudante = confirm("Você é estudante?")
+console.log(estudante)
+
+
+
+console.log("Olá, meu nome é", nome1,nome2, "eu tenho", idade, "anos de idade")
+
